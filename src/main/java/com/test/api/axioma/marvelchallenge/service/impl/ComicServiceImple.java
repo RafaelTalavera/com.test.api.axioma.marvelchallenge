5aca78630a50cc071a1e98f082c8638dac27347f
RafaelTalavera <rtalavera@live.com.ar>
2023-11-03 13:50:53 -0300

    application-dev.properties - MapperRepository

--- src/main/java/com/test/api/axioma/marvelchallenge/service/impl/ComicServiceImple.java
+++ src/main/java/com/test/api/axioma/marvelchallenge/service/impl/ComicServiceImple.java
@@ -2,20 +2,26 @@ package com.test.api.axioma.marvelchallenge.service.impl;
 
 import com.test.api.axioma.marvelchallenge.dto.MyPageable;
 import com.test.api.axioma.marvelchallenge.persistence.integration.marvel.dto.ComicDto;
+import com.test.api.axioma.marvelchallenge.persistence.integration.marvel.repository.ComicRepository;
 import com.test.api.axioma.marvelchallenge.service.ComicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ComicServiceImple implements ComicService {
+
+    @Autowired
+    private ComicRepository comicRepository;
+
     @Override
     public List<ComicDto> findAll(MyPageable pageable, Long characterId) {
-        return null;
+        return comicRepository.findAll(pageable, characterId);
     }
 
     @Override
     public ComicDto findById(Long comicId) {
-        return null;
+        return comicRepository.findByID(comicId);
     }
 }
